20659280c508dbb5fc317d8406ca340ce0c8114c
MohitAgrawal16 <mohitagrawal1684@gmail.com>
2025-01-12 21:51:26 +0530

    add persistent storge in redux

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,10 +1,20 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authSlice from './authSlice.js';
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = configureStore({
+
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, authSlice);
+
+export const store = configureStore({
     reducer: {
-        auth: authSlice
+        auth: persistedReducer
     }
 });
 
-export default store;
+export const persistor = persistStore(store);
