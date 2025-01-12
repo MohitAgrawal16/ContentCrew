eeb87aa0dca8594496b2dae5078a33121cf681e2
MohitAgrawal16 <mohitagrawal1684@gmail.com>
2025-01-12 23:42:22 +0530

    remove access token from auth slice

--- Frontend/src/store/authSlice.js
+++ Frontend/src/store/authSlice.js
@@ -3,20 +3,18 @@ import {createSlice } from '@reduxjs/toolkit';
 const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        isAuthenticated: false,
-        user: null
+        user: null,
     },
     reducers: {
         login(state, action) {
-            state.isAuthenticated = true;
             state.user = action.payload;
         },
         logout(state) {
-            state.isAuthenticated = false;
             state.user = null;
-        }
+        },
     }
 });
 
 export const {login, logout} = authSlice.actions;
+
 export default authSlice.reducer;
