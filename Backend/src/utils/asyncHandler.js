764a2f831e54633d94290385dd0395c81c416e9f
MohitAgrawal16 <mohitagrawal1684@gmail.com>
2025-01-13 00:58:57 +0530

    update asyncHandler error handling

--- Backend/src/utils/asyncHandler.js
+++ Backend/src/utils/asyncHandler.js
@@ -1,6 +1,18 @@
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+//     }
+// }
+
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next)
+    } catch (err) {
+        //console.log(err);
+        return res.status(err.statusCode || 500).json({
+            success: false,
+            message: err.message
+        })
     }
 }
 
